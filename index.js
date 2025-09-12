62909e805f631082fa921e020d0f2c1865b5bb79
Jairo Celada <jairocelada@gmail.com>
2025-09-12 15:33:23 -0400

    second commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 let notes = [
   {
@@ -29,6 +30,7 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
@@ -88,7 +90,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT | 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
